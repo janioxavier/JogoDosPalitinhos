a8036030b96d14dc6d1272891bcd49c284a4930a
janioxavier <cloud_8945@hotmail.com>
2016-05-12 15:08:52 -0300

    "Implementação pendente do BD"

--- PalitinhosServidor/src/servidor/JogoBD.java
+++ PalitinhosServidor/src/servidor/JogoBD.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
@@ -22,8 +23,34 @@ import java.util.logging.Logger;
 public class JogoBD {
     private Connection connection;
     private Statement statement;
+    private String NOME_BD = "dados_jogo";    
     
-    public JogoBD() {                
+    public JogoBD() {
+        createConnection(NOME_BD);
+        init();
+    }
+    
+    private void init() {
+        List<String> statementList = new ArrayList<>();
+        statementList.add("nome string");
+        statementList.add("palpite integer");
+        statementList.add("qtdPalitos integer");
+        statementList.add("jogadorDaVez integer"); //boolean        
+        createNewTable("Jogador", statementList);
+    }    
+    
+    public void addPlayerData(Jogador jogador) {
+        List<String> values = new ArrayList<String>();
+        values.add("'"+jogador.getNome()+"'");
+        values.add(Integer.toString(jogador.getPalpite()));
+        values.add(Integer.toString(jogador.getQtdPalitos()));
+        
+        int value = 0;
+        if (jogador.isJogadorDaVez())
+            value = 1;
+        values.add(Integer.toString(value));
+        
+        insertDataInTable("Jogador", values);
     }
     
     /**
@@ -44,25 +71,16 @@ public class JogoBD {
     /**
      * Cria uma tabela que armazenará dados do tipo informado na lista de String
      * statement. A lista deve ser formada da seguinte forma: "identificador " +
-     * " tipo". Os tipos são: "integer, string, bool, float".
+     * " tipo". Os tipos são: "integer, string, bool = [integer (0,1)]".
      * @param tableName nome da tabela que armazenara os dados     
-     * @param statement cabeçalho da tabela.      
+     * @param statementList cabeçalho da tabela.      
      */
     public void createNewTable(String tableName, List<String> statementList) {
         try {
             statement = connection.createStatement();
             statement.executeUpdate("drop table if exists " + tableName);
             
-            String statementAux = "(";
-            int sizeList = statementList.size();
-            for (int index = 0; index < sizeList; index++) {
-                statementAux += statementList.get(index);
-                if (index + 1 == sizeList)
-                    statementAux += " )";
-                else
-                    statementAux += ", ";
-            }
-            
+            String statementAux = getFormatedStatement(statementList);            
             
             statement.executeUpdate("create table " + tableName + " " + statementAux);
         } catch (SQLException ex) {
@@ -71,29 +89,46 @@ public class JogoBD {
     }
     
     /**
-     * Adiciona dados numa tabela já existente. Se o valor for do tipo String
+     * Adiciona dados numa tabela já existente ou cria uma caso ela não exista.
+     * Se o valor for do tipo String
      * então deve conter 'value' entre o valor.
      * @param tableName nome da tabela existente.
      * @param values Lista de valores a ser adicionado em dada tabela.
      */
-    public void insertDataInTable(String tableName, List<String> values) {
-        String value = "(";
-        int sizeList = values.size();
-        for (int index = 0; index < sizeList; index++) {
-            value += values.get(index);
-            if (index + 1 == sizeList)
-                value += " )";
-            else
-                value += ", ";
-            
-        }
-        try {
-            statement.executeUpdate("insert into " + tableName + " values" + value);
+    public void insertDataInTable(String tableName, List<String> values) {        
+        String formatedStatement = getFormatedStatement(values);
+        try {            
+            statement.executeUpdate("insert into " + tableName + " values" + formatedStatement);
         } catch (SQLException ex) {
             Logger.getLogger(JogoBD.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
+    public boolean containsInTable(String tableName, String identifier, String value) {
+        boolean find = false;
+        try {
+            ResultSet rs = statement.executeQuery("select * from " + tableName);
+            while(rs.next()) {
+                if (value.equals(rs.getString(identifier)))
+                    find = true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(JogoBD.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return find;
+    }
+
     
-    
+    private String getFormatedStatement(List<String> statementList) {
+        String formatedStatement = "(";
+        int sizeList = statementList.size();
+        for (int index = 0; index < sizeList; index++) {
+            formatedStatement += statementList.get(index);
+            if (index + 1 == sizeList)
+                formatedStatement += " )";
+            else
+                formatedStatement += ", ";            
+        }
+        return formatedStatement;
+    }
 }
