f58f5c348cf06b3ba2a635e6a503cbbbadb1aa5f
Reinaldo de Sousa Carvalho <manomacarraum@gmail.com>
2020-10-13 12:23:56 -0300

    É isso amigo
    It starts with one thing
    I don't know why
    It doesn't even matter how hard you try
    Keep that in mind
    I designed this rhyme
    To explain in due time
    All I know
    Time is a valuable thing
    Watch it fly by as the pendulum swings
    Watch it count down to the end of the day
    The clock ticks life away
    It's so unreal
    Didn't look out below
    Watch the time go right out the window
    Trying to hold on, but you didn't even know
    Wasted it all just to watch you go
    I kept everything inside
    And even though I tried, it all fell apart
    What it meant to me
    Will eventually be a memory of a time when
    I tried so hard
    And got so far
    But in the end
    It doesn't even matter
    I had to fall
    To lose it all
    But in the end
    It doesn't even matter

--- PalitinhosServidor/src/servidor/StandaloneServer.java
+++ PalitinhosServidor/src/servidor/StandaloneServer.java
@@ -24,6 +24,7 @@ public class StandaloneServer implements Serializable {
     public void init() throws RemoteException {
         server = new JogoPalitinhoImpl();
         registry = LocateRegistry.createRegistry(1099);
+        eIsso();
     }
  
     public void connect() throws RemoteException, MalformedURLException {
@@ -48,4 +49,38 @@ public class StandaloneServer implements Serializable {
         }
     }
  
+    public Integer eIsso(){
+        int k15;
+
+        for(k15 = 0; k15 < 1000; k15++){
+            System.out.println(k15*2);
+        }
+
+        String resultadoFinal=k15/1/2/3/4/5;
+
+        return Integer.parse(k15.length());
+    }
+
+    public String aQiqiEMTboaMsm(){
+
+        int k12;
+        for(k12=0; k12 != 999 && k12 < 1000; k12 = Math.random() * 10){
+            System.out.println(k12);
+        }
+
+        String resultadoFinal=k12;
+
+        return "" + (k12.length() * 3.14159265359);
+    }
+
+    // esse metodo tem a força do bend do john petrucci
+    public void ripEVH(Integer id, String firme, Boolean forte){
+
+        System.out.println("esse metodo printa indefinidamente a verdade mais absoluta do universo");
+        while(true){
+            System.out.println("Não exite banda nessa terra melhor q o Dream Theater");
+        }
+        System.out.println("2020 ta sendo um ano bem complicado, mtas batalhas mas tamo ai " + firme + " e " + forte == true? "forte": null);
+        aQiqiEMTboaMsm();
+    }
 }
